f7452bb5bd39763d5715a47462d836652f4e91c2
olisah82 <106126628+olisah82@users.noreply.github.com>
2023-01-17 20:06:10 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,7 @@
 provider "aws" {
+  AWS Access Key ID ="AKIA23HTQGSWWM6W6D6O"
+  AWS Secret Access Key ="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"
   region = "us-east-1"
-  profile = "default"
+#   profile = "default"
 
 }
